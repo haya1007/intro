769ad239649b6953beb8372666e0fa4d6d83a529
haya1007 <hayato.cha20021007@gmail.com>
2025-05-06 14:40:55 +0000

    トップページもろもろいい感じにできた

---
+++ haya1007.com/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
+    import "../css/global.scss";
+</script>
+
+<div class="wrapper">
+    <Header />
+
+    <slot />
+
+    <Footer />
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+</style>
